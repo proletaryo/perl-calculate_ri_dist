6e5834234eca707819e9c6cd16fccf1006891b4d
Pro Cabales <proletaryo@gmail.com>
2018-08-16 15:13:14 +0800

    added code to calculate applied RIs

--- calculate_ri_dist.pl
+++ calculate_ri_dist.pl
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 
-my $href_RISumUsageAndCost = {};
+my $href_Data = {};
 
 while (<>) {
   chomp;
@@ -11,32 +11,62 @@ while (<>) {
   # NOTE: for US-East, it's just HeavyUsage
   my $f_ProductCode = 'AmazonEC2';
   my $f_UsageType   = 'APS1-HeavyUsage';
+
+  # Get all RIs
   if ( $_ =~ /Smart.+${f_ProductCode}.+${f_UsageType}.+hourly fee/ ) {
 
-    my $ProductCode     = _extract_data( $_, 12 );    # ProductCode
-    my $UsageType       = _extract_data( $_, 15 );    # UsageType
-    my $ItemDescription = _extract_data( $_, 19 );    # ItemDescription
-    my $UsageQuantity   = _extract_data( $_, 22 );    # UsageQuantity
-    my $TotalCost       = _extract_data( $_, 29 );    # TotalCost
+    my $ProductCode   = _extract_data( $_, 12 );    # ProductCode
+    my $UsageType     = _extract_data( $_, 15 );    # UsageType
+    my $UsageQuantity = _extract_data( $_, 22 );    # UsageQuantity
+    my $TotalCost     = _extract_data( $_, 29 );    # TotalCost
 
     my $cur_SumUsageQuantity =
-      exists( $href_RISumUsageAndCost->{$ProductCode}->{$UsageType}
+      exists( $href_Data->{'HeavyUsage'}->{$ProductCode}->{$UsageType}
         ->{'SumUsageQuantity'} )
-      ? $href_RISumUsageAndCost->{$ProductCode}->{$UsageType}
+      ? $href_Data->{'HeavyUsage'}->{$ProductCode}->{$UsageType}
       ->{'SumUsageQuantity'}
       : 0;
-    $href_RISumUsageAndCost->{$ProductCode}->{$UsageType}->{'SumUsageQuantity'}
-      = $cur_SumUsageQuantity + $UsageQuantity;
+    $href_Data->{'HeavyUsage'}->{$ProductCode}->{$UsageType}
+      ->{'SumUsageQuantity'} = $cur_SumUsageQuantity + $UsageQuantity;
 
     my $cur_SumTotalCost =
       exists(
-      $href_RISumUsageAndCost->{$ProductCode}->{$UsageType}->{'SumTotalCost'} )
-      ? $href_RISumUsageAndCost->{$ProductCode}->{$UsageType}->{'SumTotalCost'}
+      $href_Data->{'HeavyUsage'}->{$ProductCode}->{$UsageType}->{'SumTotalCost'}
+      )
+      ? $href_Data->{'HeavyUsage'}->{$ProductCode}->{$UsageType}
+      ->{'SumTotalCost'}
       : 0;
-    $href_RISumUsageAndCost->{$ProductCode}->{$UsageType}->{'SumTotalCost'} =
-      $cur_SumTotalCost + $TotalCost;
+    $href_Data->{'HeavyUsage'}->{$ProductCode}->{$UsageType}->{'SumTotalCost'}
+      = $cur_SumTotalCost + $TotalCost;
 
     # print $UsageType . "\n";
+  }
+
+  # Get all usage that consumed RIs
+  elsif ( $_ =~ /${f_ProductCode}.+BoxUsage.+reserved instance applied/ ) {
+    my $LinkedAccountId   = _extract_data( $_, 2 );     # LinkedAccountId
+    my $LinkedAccountName = _extract_data( $_, 9 );     # LinkedAccountName
+    my $UsageType         = _extract_data( $_, 15 );    # UsageType
+    my $UsageQuantity     = _extract_data( $_, 22 );    # UsageQuantity
+    my $CostCodeCategory  = _extract_data( $_, 32 );    # CostCodeCategory
+
+    my ( $RegionUsageType, undef ) = split( /:/, $UsageType );
+    my $RIAppliedType = _get_RI_applied_type($_);
+
+    $href_Data->{'BoxUsage'}->{$RegionUsageType}->{$RIAppliedType}
+      ->{$LinkedAccountId}->{'CostCodeCategory'} = $CostCodeCategory;
+    $href_Data->{'BoxUsage'}->{$RegionUsageType}->{$RIAppliedType}
+      ->{$LinkedAccountId}->{'LinkedAccountName'} = $LinkedAccountName;
+
+    my $cur_UsageQuantity =
+      exists( $href_Data->{'BoxUsage'}->{$RegionUsageType}->{$RIAppliedType}
+        ->{$LinkedAccountId}->{'UsageQuantity'} )
+      ? $href_Data->{'BoxUsage'}->{$RegionUsageType}->{$RIAppliedType}
+      ->{$LinkedAccountId}->{'UsageQuantity'}
+      : 0;
+    $href_Data->{'BoxUsage'}->{$RegionUsageType}->{$RIAppliedType}
+      ->{$LinkedAccountId}->{'UsageQuantity'} =
+      $cur_UsageQuantity + $UsageQuantity;
   }
 
 }
@@ -56,4 +86,16 @@ sub _extract_data {
   if ( not $type ) { die }
 
   return $type;
+}
+
+sub _get_RI_applied_type {
+  my $d     = shift @_;
+  my $value = undef;
+
+  my $ItemDescription = _extract_data( $d, 19 );
+  if ( $ItemDescription =~ /, (.+) reserved instance applied/ ) {
+    $value = $1;
+  }
+
+  return $value;
 }
